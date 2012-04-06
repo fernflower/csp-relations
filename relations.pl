8d09d3f27139ce8aa05d3f217a9506a3475e2d17
ina <inavasilevsakaya@gmail.com>
2012-04-06 23:18:36 +0400

    -p & -c cmd options

--- relations.pl
+++ relations.pl
@@ -4,25 +4,74 @@ use Data::Dumper;
 
 # parse cmd opts
 my %opts;
-getopts(':1:2:o:', \%opts);
+getopts(':1:2:o:c:p:', \%opts);
 
-$opts{o}|= "out";
+$opts{o}||= "out";
+$opts{p}||= "join";
 
-
-if (!defined $opts{1} || !defined $opts{2}){
-    print "Invalid usage\n";
-    HELP();
-    exit 1;
+if ($opts{p} eq "join") {
+    if (!defined $opts{1} || !defined $opts{2} || !defined $opts{c}){
+        print "Invalid usage, choose 2 relations for JOIN and a JOIN-condition\n";
+        HELP();
+        exit 1;
+    }
+    my @rel1, @rel2;
+    my @vars1, @vars2;
+    my $join, $joinVars;
+    FORM_MATRIX(\@rel1, \@vars1 ,$opts{1});
+    FORM_MATRIX(\@rel2, \@vars2 ,$opts{2});
+    (my $var, my $value) = PARSE_CONDITION($opts{c});
+    if (!defined $value) {
+        ($join, $joinVars) = JOIN(\@rel1, \@rel2, \@vars1, \@vars2, $var);
+    }
+    else {
+        ($join, $joinVars) = JOIN(\@rel1, \@rel2, \@vars1, \@vars2, "$var=$value");
+    }
+    PRINT_OUT($join, $joinVars);
 }
 
+if ($opts{p} eq "cartesian") {
+    if (!defined $opts{1} || !defined $opts{2}){
+        print "Invalid usage, choose 2 relations for CARTESIAN\n";
+        HELP();
+        exit 1;
+    }
+    my @rel1, @rel2, @vars1, @vars2;
+    my $cartesian, $cartesianVars;
+    FORM_MATRIX(\@rel1, \@vars1 ,$opts{1});
+    FORM_MATRIX(\@rel2, \@vars2 ,$opts{2});
+    ($cartesian, $cartesianVars) = CARTESIAN(\@rel1, \@rel2, \@vars1, \@vars2);
+    PRINT_OUT($cartesian, $cartesianVars);
+}
 
-my @rel1, @rel2;
-my @vars1, @vars2;
-FORM_MATRIX(\@rel1, \@vars1 ,$opts{1});
-FORM_MATRIX(\@rel2, \@vars2 ,$opts{2});
+if ($opts{p} eq "project") {
+    if (!defined $opts{1}){
+        print "Invalid usage, choose 1 relation for PROJECT and a PROJECT-condition\n";
+        HELP();
+        exit 1;
+    }
+    my @rel1, @vars1;
+    my $project, $projectVars;
+    FORM_MATRIX(\@rel1, \@vars1 ,$opts{1});
+    my $var = $opts{c};
+    ($project, $projectVars) = PROJECT(\@rel1, \@vars1, $opts{c});
+    PRINT_OUT($project, $projectVars);
+}
 
-(my $join, my $joinVars) = JOIN(\@rel1, \@rel2, \@vars1, \@vars2, "z");
-PRINT_OUT($join, $joinVars);
+if ($opts{p} eq "select") {
+    if (!defined $opts{1}){
+        print "Invalid usage, choose 1 relation for SELECT and a SELECT-condition\n";
+        HELP();
+        exit 1;
+    }
+    my @rel1, @vars1;
+    my $select, $selectVars;
+    FORM_MATRIX(\@rel1, \@vars1 ,$opts{1});
+    (my $var, my $value) = PARSE_CONDITION($opts{c});
+    $select = SELECT(\@rel1, \@vars1, "$var=$value");
+    PRINT_OUT($select, \@vars1);
+}
+
 
 sub FORM_MATRIX {
     (my $relation, my $vars, my $filename) = @_;
@@ -56,12 +105,15 @@ sub CONDITIONAL_JOIN {
     (my $condVar, my $value) = PARSE_CONDITION($condition);
     #select all $condVar = $value from relation
     (my $newRel1) = SELECT($rel1, $vars1, $condition);
-    (my $cutNewRel1, my $newVars1) = CUT($newRel1, $vars1, $condVar);
+    (my $cutNewRel1, my $newVars1) = PROJECT($newRel1, $vars1, $condVar);
     (my $newRel2) = SELECT($rel2, $vars2, $condition);
+    print Dumper($newRel2);
     (my $relation, my $vars) = CARTESIAN($cutNewRel1, $newRel2, $newVars1, $vars2);
+    print Dumper($vars);
     return ($relation, $vars); 
 }
 
+#TO DO: support other domains (now D=[0,1] only is supported)
 #condVar is a string like "k"
 sub JOIN {
     (my $rel1, my $rel2, my $vars1, my $vars2, my $condVar) = @_;
@@ -108,7 +160,7 @@ sub SELECT {
 }
 
 # projection: remove $cutVar from the relation
-sub CUT {
+sub PROJECT {
     (my $relation, my $vars, my $cutVar) = @_;
     my @result;
     my @resVars;
@@ -191,6 +243,8 @@ sub HELP {
     -1 - first relation's table
     -2 - second relations's table
     -o - output
+    -p - perform operation: "join", "project", "cartesian", "select" ("join" by default)
+    -c - condition ("x", "x=1")
 
     Typical relation table format: 
     x; 0 0 1 1
